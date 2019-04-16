feec75b6d6c672a3025971df1e9f876f56182d3e
enriqueescobar-askida <enrique.escobar@askida.com>
2019-04-16 16:32:04 -0400

    update

--- WpfApp/WpfApp/MainWindow.xaml.cs
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -5,8 +5,6 @@
 * OR 4/10/2019 6:35:18 PM
 **/
 
-using WpfApp.DataAccessLayer.Jobs;
-
 namespace WpfApp
 {
     using DataAccessLayer.Files;
@@ -18,6 +16,8 @@ namespace WpfApp
     using System.Windows.Controls;
     using System.Windows.Input;
 
+    using WpfApp.DataAccessLayer.Jobs;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -25,7 +25,7 @@ namespace WpfApp
     {
         #region PrivateAttributes
         /// <summary>The thickness</summary>
-        private readonly double _thickness = 50.0;
+        private readonly double _thickness = 10.0;
         #endregion
 
         #region Properties
@@ -49,22 +49,73 @@ namespace WpfApp
             this.Title = "Amaris Consulting: International consulting company | " + ToString().Split('.')[1];
             this.Left = this._thickness;
             this.Top = this._thickness;
-            this.Height = this._thickness * 12.0;
-            this.Width = this._thickness * 16.0;
+            this.Height = this._thickness * 60.0;
+            this.Width = this._thickness * 80.0;
             this.CsvFile = new CsvFile();
             this.InitializeComponent();
-            this.WpfAppMainStatusBarProgressBar.Value = this._thickness / 2.0;
+            this.WpfAppMainStatusBarProgressBar.Value = this._thickness * 2.5;
+            this.InitializeImage();
+            this.InitializeLabelBanner();
+            this.InitializeDockPanelUp();
+            this.InitializeDockPanelBottom();
+            this.InitializeWpfAppMainListBox();
+        }
+
+        /// <summary>Initializes the image.</summary>
+        private void InitializeImage()
+        {
+            this.WpfMainImage.Height = this._thickness * 2.0;
+            this.WpfMainImage.Margin = new Thickness(this._thickness, this._thickness, 0.0, 0.0);
+        }
+
+        /// <summary>Initializes the label banner.</summary>
+        private void InitializeLabelBanner()
+        {
+            this.WpfMainLabelBanner.Height = this._thickness * 2.5;
+            this.WpfMainLabelBanner.Width = this._thickness * 60.0;
+            this.WpfMainLabelBanner.Margin = new Thickness(this._thickness * 15.0, this._thickness, 0.0, 0.0);
+            this.WpfMainLabelBanner.FontSize = this._thickness * 1.2;
+        }
+
+        /// <summary>Initializes the dock panel up.</summary>
+        private void InitializeDockPanelUp()
+        {
+            this.WpfMainDockPanelUp.Height = this._thickness * 2.0;
+            this.WpfMainDockPanelUp.Margin =
+                new Thickness(this._thickness, this._thickness * 5.0, this._thickness, this._thickness * 20.0);
+            this.WpfAppMainOpen.Width = this._thickness * 14.0;
+            this.WpfAppMainSave.Width = this._thickness * 14.0;
+            this.WpfAppMainExit.Width = this._thickness * 14.0;
+            this.WpfAppMainAbout.Width = this._thickness * 14.0;
+        }
+
+        /// <summary>Initializes the dock panel bottom.</summary>
+        private void InitializeDockPanelBottom()
+        {
+            this.WpfAppMainStatusBarTextBlockLeft.Text = "Byte size: ";
+            this.WpfAppMainStatusBarTextBlockCenter.Text = "File path: ";
+            this.WpfAppMainStatusBarProgressBar.Height = this._thickness * 2.0;
+            this.WpfAppMainStatusBarProgressBar.Width = this._thickness * 9.0;
+        }
+
+        /// <summary>Initializes the WPF application main ListBox.</summary>
+        private void InitializeWpfAppMainListBox()
+        {
+            this.WpfAppMainListBox.SelectionMode = SelectionMode.Single;
+            this.WpfAppMainListBox.DisplayMemberPath = "Url";
+            this.WpfAppMainListBox.MouseDoubleClick += WpfAppMainListBox_OnMouseDoubleClick;
+            double d = this._thickness / 4.0;
+            this.WpfAppMainListBox.BorderThickness = new Thickness(d, d, d, d);
+            this.WpfAppMainListBox.Height = this._thickness * 44.5;
+            this.WpfAppMainListBox.Width = this._thickness * 76.5;
+            this.WpfAppMainListBox.Margin = new Thickness(this._thickness, this._thickness * 8.0, 0.0, 0.0);
         }
         #endregion
 
         /// <summary>Initializes the WPF application main ListBox.</summary>
         /// <param name="urlLinks">The URL links.</param>
-        private void InitializeWpfAppMainListBox(List<UrlLink> urlLinks)
+        private void PopulateListBox(List<UrlLink> urlLinks)
         {
-            this.WpfAppMainListBox.SelectionMode = SelectionMode.Single;
-            this.WpfAppMainListBox.DisplayMemberPath = "Url";
-            this.WpfAppMainListBox.MouseDoubleClick += WpfAppMainListBox_OnMouseDoubleClick;
-
             foreach (UrlLink urlLink in urlLinks)
                 this.WpfAppMainListBox.Items.Add(urlLink);
             /*object selectedItem = this.WpfAppMainListBox.SelectedItem;
@@ -108,6 +159,7 @@ namespace WpfApp
                     Clipboard.SetText(urlLink.Link);
                     // System.Diagnostics.Process.Start("iexplore.exe", "http://www.msn.com");Process.Start(urlLink.Link);
                     WebJobPosting webJobPosting = new WebJobPosting(urlLink.Url);
+                    string w = webJobPosting.Html;
                 }
         }
 
@@ -130,19 +182,19 @@ namespace WpfApp
             if (result != null && result == true)
             {
                 this.CsvFile.SetFileInfo(new FileInfo(this.OpenFileDialog.FileName));
-                this.WpfAppMainStatusBarTextBlockCenter.Text = this.CsvFile.FileInfo.FullName;
+                this.WpfAppMainStatusBarTextBlockCenter.Text +=
+                    this.CsvFile.FileInfo.FullName + " ".PadRight(15, '.') + " Readable: ";
                 this.WpfAppMainStatusBarProgressBar.Value = 30.0;
 
                 if (this.CsvFile.Read())
                 {
                     this.WpfAppMainStatusBarProgressBar.Value = 80.0;
-                    this.WpfAppMainStatusBarTextBlockLeft.Text = this.CsvFile.FileInfo.Length + " bytes.";
-                    this.WpfAppMainStatusBarTextBlockCenter.Text += " ".PadRight(12, '.');
-                    this.WpfAppMainStatusBarTextBlockCenter.Text += this.CsvFile.IsReadable ? " is " : " is not ";
-                    this.WpfAppMainStatusBarTextBlockCenter.Text += "readable with " + this.CsvFile.Rows + " rows ";
-                    this.WpfAppMainStatusBarTextBlockCenter.Text += this.CsvFile.URLs.Count + " results.";
+                    this.WpfAppMainStatusBarTextBlockLeft.Text += this.CsvFile.FileInfo.Length;
+                    this.WpfAppMainStatusBarTextBlockCenter.Text += this.CsvFile.IsReadable ? "Yes" : "No";
+                    this.WpfAppMainStatusBarTextBlockCenter.Text += " Rows: " + this.CsvFile.Rows;
+                    this.WpfAppMainStatusBarTextBlockCenter.Text += " Results: " + this.CsvFile.URLs.Count;
                     this.WpfAppMainStatusBarProgressBar.Value = 100.0;
-                    this.InitializeWpfAppMainListBox(this.CsvFile.URLs);
+                    this.PopulateListBox(this.CsvFile.URLs);
                 }
             }
         }
