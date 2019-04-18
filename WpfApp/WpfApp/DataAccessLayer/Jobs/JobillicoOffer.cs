e24fcda3c4e49da72fd733c6bcc004546e9992ba
enriqueescobar-askida <enrique.escobar@askida.com>
2019-04-18 18:54:22 -0400

    update with new html agility pack & newtonsoft json

--- WpfApp/WpfApp/DataAccessLayer/Jobs/JobillicoOffer.cs
+++ WpfApp/WpfApp/DataAccessLayer/Jobs/JobillicoOffer.cs
@@ -43,8 +43,40 @@ namespace WpfApp.DataAccessLayer.Jobs
         /// <param name="bodyHtmlNode">The bodyHtmlNode<see cref="HtmlNode"/></param>
         public JobillicoOffer(HtmlNode bodyHtmlNode)
         {
-            var v = bodyHtmlNode;
+            HtmlNode jobHtmlNode = this.GetHtmlNodeFromDivIdInBodyHtmlNode(bodyHtmlNode, "printJobSection");
+            HtmlNode companyHtmlNode = this.GetHtmlNodeFromH2ClassInJobHtmlNode(jobHtmlNode, "main-article-content col-md-8");
+            this.MetaTitle = this.GetMetaTitleFromJobHtmlNode(jobHtmlNode, "h1-class art-head");
+            var v = this.GetFromHtml(companyHtmlNode);
         }
+
+        private HtmlNode GetFromHtml(HtmlNode companyHtmlNode)
+        {
+            return companyHtmlNode.ChildNodes.FindFirst("article").FirstChild;
+        }
+
+        /// <summary>Gets the HTML node from h2 class in job HTML node.</summary>
+        /// <param name="jobHtmlNode">The job HTML node.</param>
+        /// <param name="h2Class">The h2 class.</param>
+        /// <returns></returns>
+        private HtmlNode GetHtmlNodeFromH2ClassInJobHtmlNode(HtmlNode jobHtmlNode, string h2Class)
+            => jobHtmlNode.SelectSingleNode("//div[@class ='" + h2Class + "']");
+
+        /// <summary>Gets the meta title from job HTML node.</summary>
+        /// <param name="jobHtmlNode">The job HTML node.</param>
+        /// <param name="h1Class"></param>
+        /// <returns>String on inner text</returns>
+        private string GetMetaTitleFromJobHtmlNode(HtmlNode jobHtmlNode, string h1Class)
+            => jobHtmlNode.SelectSingleNode("//h1[@class ='" + h1Class + "']").InnerText.TrimStart().TrimEnd().Trim();
+
+        #endregion
+
+        #region PrivateMethods
+        /// <summary>Gets the HTML node from div identifier in body HTML node.</summary>
+        /// <param name="bodyHtmlNode">The body HTML node.</param>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        private HtmlNode GetHtmlNodeFromDivIdInBodyHtmlNode(HtmlNode bodyHtmlNode, string id)
+            => bodyHtmlNode.SelectSingleNode("//div[@id ='" + id + "']");
         #endregion
     }
 }
